6029ad76f0f844600a84a6fc6b8dc025d30de783
Andrew.Easterling <Anddrew914@gmail.com>
2017-04-11 19:45:51 -0400

    Adds alternating x's and ohs

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -3,21 +3,43 @@ const winning = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
                  [0, 3, 6], [1, 4, 7], [2, 5, 8],
                  [0, 4, 8], [6, 4, 2]]
 
-const x = []
+const xArray = []
+const oArray = []
 
-const winner = function () {
+let currentplayer = 'x'
+
+const xwinner = function () {
   $.each(winning, function (index, value) {
-    if (x.includes(value[0]) &&
-      x.includes(value[1]) &&
-      x.includes(value[2])) {
+    if (xArray.includes(value[0]) &&
+      xArray.includes(value[1]) &&
+      xArray.includes(value[2])) {
       console.log('winner!')
     }
   })
 }
-// Changes image
+
+const owinner = function () {
+  $.each(winning, function (index, value) {
+    if (oArray.includes(value[0]) &&
+      oArray.includes(value[1]) &&
+      oArray.includes(value[2])) {
+      console.log('winner!')
+    }
+  })
+}
+
+// Click controller
 $('#playarea').on('click', 'img', function (evt) {
-  $(this).attr('src', 'assets/styles/images/ohs.png')
   const idnumber = $(evt.target).data('id')
-  x.push(idnumber)
-  return winner()
+  if (currentplayer === 'x') {
+    $(this).attr('src', 'assets/styles/images/exes.png')
+    xArray.push(idnumber)
+    currentplayer = 'o'
+    return xwinner()
+  } else {
+    $(this).attr('src', 'assets/styles/images/ohs.png')
+    oArray.push(idnumber)
+    currentplayer = 'x'
+    return owinner()
+  }
 })
