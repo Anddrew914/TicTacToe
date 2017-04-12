2dfa6a9760dea15f866fdfec7589d3b3c49bf0cd
Andrew.Easterling <Anddrew914@gmail.com>
2017-04-12 10:50:52 -0400

    Adds game API folder to begin process of building game API

---
+++ assets/scripts/gameAPI/events.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const getFormFields = require(`../../../lib/get-form-fields`)
+
+const api = require('./api')
+const ui = require('./ui')
+
+const onSignUp = function (event) {
+  const data = getFormFields(this)
+  event.preventDefault()
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = function (event) {
+  const data = getFormFields(this)
+  event.preventDefault()
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  console.log('inside')
+  const data = getFormFields(event.target)
+
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+  console.log('logOut')
+  const data = getFormFields(event.target)
+
+  api.signOut(data)
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#change-password').on('submit', onChangePassword)
+  $('#sign-out').on('submit', onSignOut)
+}
+
+module.exports = {
+  addHandlers
+}
---
+++ assets/scripts/gameAPI/ui.js
@@ -0,0 +1,53 @@
+'use strict'
+const store = require('../store.js')
+const signUpSuccess = (data) => {
+  console.log(data)
+  $('#sign-up').hide()
+}
+
+const signUpFailure = (error) => {
+  console.error(error)
+}
+
+const signInSuccess = (data) => {
+  console.log(data)
+  store.user = data.user
+  $('#playarea').toggle()
+  $('#sign-in').hide()
+  $('#sign-up').hide()
+  $('#sign-out').show()
+}
+
+const signInFailure = (error) => {
+  console.error(error)
+}
+
+const changePasswordSuccess = (data) => {
+  console.log('data is', data)
+}
+
+const changePasswordFailure = (error) => {
+  console.log(error)
+}
+
+const signOutSuccess = (data) => {
+  console.log('data is sign', data)
+  $('#playarea').hide()
+  $('#sign-in').show()
+  $('#sign-up').show()
+  $('#sign-out').hide()
+}
+
+const signOutFailure = (error) => {
+  console.log(error)
+}
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
